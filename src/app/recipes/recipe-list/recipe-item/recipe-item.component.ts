27b5ab488d01c1ae244abf6f1903869aed7242a3
Davide <davidelombardo99@gmail.com>
2018-12-04 17:22:44 +0100

    altre modifiche compà

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, Input } from "@angular/core";
 import { Recipes } from "../recipe.model";
 
 @Component({
@@ -7,7 +7,7 @@ import { Recipes } from "../recipe.model";
   styleUrls: ["./recipe-item.component.scss"]
 })
 export class RecipeItemComponent implements OnInit {
-  currentRicetta: Recipes;
+  @Input() currentRicetta: Recipes;
   constructor() {}
 
   ngOnInit() {}
